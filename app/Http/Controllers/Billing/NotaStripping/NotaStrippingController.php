4bd734a490a5eda0dc637ce0be54c450cb5f9e74
rifqyar <rifqyaditya55@gmail.com>
2025-02-25 14:18:47 +0700

    a

--- app/Http/Controllers/Billing/NotaStripping/NotaStrippingController.php
+++ app/Http/Controllers/Billing/NotaStripping/NotaStrippingController.php
@@ -213,8 +213,8 @@ class NotaStrippingController extends Controller
         $no_req = base64_decode($no_req);
         $query_cek_nota 	= "SELECT NO_NOTA, STATUS FROM NOTA_RELOKASI_MTY WHERE NO_REQUEST = '$no_req'";
         $nota				= DB::connection('uster')->selectOne($query_cek_nota);
-        $no_nota_cek		= $nota->no_nota;
-        $nota_status		= $nota->status;
+        $no_nota_cek		= $nota->no_nota ?? null;
+        $nota_status		= $nota->status ?? null;
 
         try {
             if (($no_nota_cek != NULL && $nota_status == 'BATAL') || ($no_nota_cek == NULL && $nota_status == NULL)){

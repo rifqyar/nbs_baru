865fb4769fc4d560b512d953a46d21b3f165906b
rifqyar <rifqyaditya55@gmail.com>
2025-03-12 10:46:11 +0700

    s

--- resources/views/billing/stuffing/plan/print/previewproforma.blade.php
+++ resources/views/billing/stuffing/plan/print/previewproforma.blade.php
@@ -231,7 +231,7 @@
                                             <div class="col-lg-1 col-md-1 d-none d-md-block d-lg-block">:</div>
                                             <div class="col-lg-4 col-md-12 text-right">
                                                 <span class="text-dark font-medium">
-                                                    {{ number_format($row_materai, 0, ',', '.') }}
+                                                    {{ $row_materai }}
                                                 </span>
                                             </div>
                                         </div>

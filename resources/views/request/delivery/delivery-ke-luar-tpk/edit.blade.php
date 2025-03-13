75823914d7c99521748f1cfd16c7c5e810125795
rifqyar <rifqyaditya55@gmail.com>
2025-03-13 14:24:27 +0700

    s

--- resources/views/request/delivery/delivery-ke-luar-tpk/edit.blade.php
+++ resources/views/request/delivery/delivery-ke-luar-tpk/edit.blade.php
@@ -855,8 +855,8 @@
                     $(row).append('<input type="hidden" id="NO_CONT_' + rowIdx +
                         '" class="hidden-input" value="' + data.no_container + '">');
                 },
-                lengthMenu: [10, 20, 50, 100], // Set the default page lengths
-                pageLength: 10, // Set the initial page length
+                lengthMenu: [10, 20, 50, 100, 200], // Set the default page lengths
+                pageLength: 200, // Set the initial page length
                 initComplete: function() {
                     var table = $('#container-table').DataTable();
                     var totalRows = table.rows().count();

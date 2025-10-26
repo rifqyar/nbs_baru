a36375527c432755dd6ef1aa3a496f58095b5ec0
rifqyar <rifqyaditya55@gmail.com>
2025-10-26 17:45:57 +0700

    debug return dta nota delivery

--- app/Services/Billing/NotaDelivery/NotaDeliveryService.php
+++ app/Services/Billing/NotaDelivery/NotaDeliveryService.php
@@ -296,7 +296,6 @@ class NotaDeliveryService
                 jenis := 'delivery';
                 pack_get_nota_delivery_tpk.create_detail_nota(id_nota,tgl_req,no_request,jenis, err_msg);
             END;";
-            dd($sql_xpi);
             DB::connection('uster')->statement($sql_xpi);
             //  echo $sql_xpi;die;
         } else {
@@ -474,6 +473,7 @@ class NotaDeliveryService
             "rnota" => ''
         ];
 
+        dd($dataArr);
         return $dataArr;
     }
 
